25ddc1a7999a358958092550fed87b865c140701
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-03-08 13:13:08 +0530

    basics

---
+++ React/basics.js
@@ -0,0 +1,16 @@
+
+/*
+in react 
+yaha kaafi sari files bn jati ha 
+
+node_modules means the modules we want to install indivisually 
+gitignore helps us not to uplaod these node_modules on github
+package.json helps us have the same version 
+
+
+//
+React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.
+
+
+
+*/
