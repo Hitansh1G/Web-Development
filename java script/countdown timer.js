623d55591202af175be6987d8ebef45056f8fc9d
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-23 14:28:33 +0530

    countdown timer

---
+++ java script/countdown timer.js
@@ -0,0 +1,11 @@
+var count =10;
+function timer(){
+    if(count==0){
+        console.log("Time up ");
+        clearInterval(id);
+        return;
+    }
+    console.log(count);
+    count--;
+}
+var id = setInterval(timer , 500);
