897435f1c1e5c6506faee6a3464b5726c733eb20
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-04 12:37:09 +0530

    smooth scroll process 1

---
+++ java script/smooth scroll.js
@@ -0,0 +1,10 @@
+var targetPos = 1500;
+var currentPos = 0;
+var scrollInterval = setInterval(function(){
+    if(currentPos>=targetPos){
+        clearInterval(scrollInterval);
+        return;
+    }
+    currentPos += 50;
+    window.scrollBy(0,50);
+},50);
