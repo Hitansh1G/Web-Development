18a4a8718301b8fc8a07e49bb3e9b570ddd9d6cd
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-16 17:24:50 +0530

    Promises

--- java script/promises.js
+++ java script/promises.js
@@ -5,5 +5,17 @@ var promise = new Promise((resolve,reject)=>{
         resolve({message:'success'});
     },3000);
 });
-   
-console.log(promise);
+//the promise will be in the pending state , after 3 sec when we open it.. it will be in fulfilled state .. we have correctly amulated a api call . when we send an api call after a certain amount of time it shows success 
+// what to do now 
+promise
+    .then((data) =>{
+        //lets say we have an api .. we have called that data , now how will u access that data .. this above function helps in that
+    console.log(data);
+    })
+    //we can chain the methods like this
+    .catch((error)=>{
+        //this will be called whenever the promise will be rejected
+        console.log('error',error);
+    });
+
+
