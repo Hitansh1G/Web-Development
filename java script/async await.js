1b373223ffd13c39d6c026837db5bf7cb5d0799e
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-17 11:42:30 +0530

    async await

--- java script/async await.js
+++ java script/async await.js
@@ -12,3 +12,15 @@ async function asynctask(){
     return "resolved";
 }
 //whenever i write async in front of a function it implies that the function will return a promise
+asynctask().then(data=>console.log(data))
+// also a way 
+
+function getData(){
+    return Promise.resolve('some data');
+}
+async function abc(){
+    const data = await getData();
+};
+//this absically means that wenevre we call the abc function , get data function will be called , it will bascailly get a prmise , ans as soon as the java scipt encounters the await keyword , it will first suspend the function , in the background after the function is suspended we will wait for the getData task to be completed  and then java script will move on the syncronous code 
+
+
