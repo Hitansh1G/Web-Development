bd477fe85f35d8658e98071748ec5f1cb9996f5d
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-06-17 17:33:42 +0530

    SPA and MPA

---
+++ java script/SPA and MPA.js
@@ -0,0 +1,9 @@
+//there are 2 ways to build a java script project 
+//first is to make a single page application 
+//second is to make multi page application
+
+/*
+so in a multi page application is made of several pages that contains static content like text,images , links to other pages and so no 
+while we are browsing through the app  the  browser will reload the resources like js and css aagain and again even if some of the componenets are same
+
+*/
